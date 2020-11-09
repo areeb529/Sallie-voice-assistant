9b43a1357c9fb9c34aac8ae93105d45dd975e01c
Faisal Jamil <69186619+faisaljamil25@users.noreply.github.com>
2020-11-10 01:16:21 +0530

    added respond commands

--- main.py
+++ main.py
@@ -15,11 +15,17 @@ rate = speaker.getProperty('rate')
 speaker.setProperty('rate', 150)
 
 
+class Person:
+    name = ''
+
+    def set_name(self, name):
+        self.name = name
+
+
 def record_audio(query=False):
     with sr.Microphone() as source:     # microphone as source
         if query:
             sallie_speak(query)
-            print(query)
         audio = r.listen(source)    # listen for the audio via source
         voice_data = ''
         try:
@@ -30,22 +36,69 @@ def record_audio(query=False):
         except sr.RequestError:
             # error: recognizer is not connected
             sallie_speak('Sorry, I cannot process your request at the moment')
-        print(f">> {voice_data.lower()}")  # print what user said
+        print(f"Me: {voice_data.lower()}")  # print what user said
         return voice_data
 
 
 def sallie_speak(audio_string):
-    print('Computer: ' + audio_string)
+    print('Saillie: ' + audio_string)
     speaker.say(audio_string)
     speaker.runAndWait()
 
 
+def there_exists(words):
+    for word in words:
+        if word in voice_data:
+            return True
+
+
 def respond(voice_data):
-    if 'what is your name' in voice_data:
-        sallie_speak('My name is Sallie')
-        print('My name is Sallie')
-    if 'what time is it' in voice_data:
-        sallie_speak(ctime())
+    # 1: greeting
+    if there_exists(['hello', 'hi', 'hey']):
+        greetings = [f"hey, how can I help you {person_obj.name}", f"hey, what's up? {person_obj.name}",
+                     f"I'm listening {person_obj.name}", f"hello {person_obj.name}", f"It's really good to hear from you {person_obj.name}"]
+        greet = greetings[random.randint(0, len(greetings)-1)]
+        sallie_speak(greet)
+
+    if there_exists(["how are you", "how are you doing"]):
+        sallie_speak(f"I'm very well, thanks for asking {person_obj.name}")
+
+    if there_exists(["what's up"]):
+        sallie_speak(
+            "Not much! Just been looking into ways to stay healthy. I learned that wearing a mask in public saves lives")
+        sallie_speak("Hope you are rocking one!")
+
+    # 2: name
+    if there_exists(["what is your name", "what's your name", "tell me your name"]):
+        if person_obj.name:
+            sallie_speak("my name is Sallie")
+        else:
+            sallie_speak("my name is Sallie. what's your name?")
+
+    if there_exists(["my name is"]):
+        person_name = voice_data.split("is")[-1].strip()
+        sallie_speak(f"okay, i will remember that {person_name}")
+        person_obj.setName(person_name)  # remember name in person object
+
+    if there_exists(["No thats wrong name"]):
+        sallie_speak("what should I call you?")
+        person_name = record_audio()
+        sallie_speak(f"Nice to meet you {person_name}")
+
+    # 4: time
+    if there_exists(["what's the time", "tell me the time", "what time is it"]):
+        time = ctime().split(" ")[3].split(":")[0:2]
+        if time[0] == "00":
+            hours = "12"
+        else:
+            hours = time[0]
+        minutes = time[1]
+        time = f"{hours} {minutes}"
+        sallie_speak(time)
+
+    if there_exists(["exit", "quit", "goodbye"]):
+        sallie_speak("going offline")
+        exit()
     if 'search' in voice_data:
         search = record_audio('What do you want to search for?')
         url = 'https://google.com/search?q=' + search
@@ -58,14 +111,11 @@ def respond(voice_data):
         webbrowser.get().open(url)
         sallie_speak('Here is the location for ' + location)
         print('Here is the location for ' + location)
-    if 'exit' in voice_data:
-        sallie_speak('Thank You')
-        print('Thank you!')
-        exit()
 
 
+person_obj = Person()
 time.sleep(1)
 sallie_speak('How can I help you?')
 while 1:
-    voice_data = record_audio()
-    respond(voice_data)
+    voice_data = record_audio()     # get the voice input
+    respond(voice_data)     # respond
